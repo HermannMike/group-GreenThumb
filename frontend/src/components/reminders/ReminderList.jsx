82cd1a170b34f4d7f89693bfd2bc094c03366d98
Mosesotieno-ai <mosesotieno8363@gmail.com>
2025-06-26 15:21:34 +0300

    refactor: consolidate ReminderItem into ReminderList component

--- frontend/src/components/reminders/ReminderList.jsx
+++ frontend/src/components/reminders/ReminderList.jsx
@@ -1,8 +1,23 @@
-const ReminderItem = ({ reminder, onDelete }) => (
-  <li>
-    {reminder.text} - {reminder.date}
-    <button onClick={() => onDelete(reminder.id)}>Delete</button>
-  </li>
-);
+import { useState } from "react";
+import ReminderForm from "./ReminderForm";
+import ReminderItem from "./ReminderItem";
 
-export default ReminderItem;
+const ReminderList = () => {
+  const [reminders, setReminders] = useState([]);
+
+  const addReminder = (reminder) => setReminders([...reminders, reminder]);
+  const deleteReminder = (id) => setReminders(reminders.filter((r) => r.id !== id));
+
+  return (
+    <div>
+      <ReminderForm onAdd={addReminder} />
+      <ul>
+        {reminders.map((reminder) => (
+          <ReminderItem key={reminder.id} reminder={reminder} onDelete={deleteReminder} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ReminderList;
